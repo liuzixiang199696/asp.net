eb65e3343535a3d6e60c40bb186c9fd3e2bf170d
zx <zx@DESKTOP-81P9SJC>
2019-06-20 20:04:20 +0800

    1

--- yuanzhan/yuanzhan/17bang/User.cs
+++ yuanzhan/yuanzhan/17bang/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace assignment._17ban
 {
@@ -16,7 +17,33 @@ namespace assignment._17ban
         {
             Name = name;
         }
+        internal static void users()
+        {
+            XElement element = new XElement("Users",
+                new XElement("User1",
+                new XElement("id", 1),
+                new XElement("name", "feige"),
+                new XElement("password", "123456")),
+                new XElement("User2",
+                new XElement("id", 2),
+                new XElement("name", "laocheng"),
+                new XElement("password", "1234567")),
+                new XElement("User3",
+                new XElement("id", 12),
+                new XElement("name", "zixiang"),
+                new XElement("password", "12345678"))
+                );
+            XDocument document = new XDocument(element);
+            document.Save("G:\\xiangmu\\Users.xml");
 
-       
+        }
+        //删除没有发表文章的用户
+        internal static void DeleteUser()
+        {
+            XElement User = XElement.Load(" G:\\xiangmu\\Users.xml");
+            XElement element = XElement.Load("G:\\xiangmu\\articles.xml");
+        }
+
+
     }
 }
