d0bd1744c102e31c69fa64645396a27b58fb530d
zx <zx@DESKTOP-81P9SJC>
2019-06-24 20:19:34 +0800

    1

---
+++ yuanzhan/yuanzhan/IO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace yuanzhan
+{
+    class IO
+    {
+        private static string Path = @"G:\xiangmu\测试邮箱.txt";
+
+        static string GetFromTxt(string Path)
+        {
+            string str = File.ReadAllText(Path);
+            return str;
+        }
+
+        static string Filter()//过滤器
+        {
+
+
+            return null;
+        } //过滤器
+
+        static string RemoveRepeatTest()
+        {
+            return null;
+        }   //移除重复
+
+        static void SaveInGroup() { } //保存
+
+    }
+}
