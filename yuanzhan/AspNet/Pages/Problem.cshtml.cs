5cb39f2438f1f093e7690c80c8849ade28b95bd2
zx <zx@DESKTOP-81P9SJC>
2019-07-06 22:27:59 +0800

    睡觉前提交代码

---
+++ yuanzhan/AspNet/Pages/Problem.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RazorPage.Pages
+{
+    public class ProblemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
