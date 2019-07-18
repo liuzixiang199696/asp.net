9342c9518b54c4d4bda3a284dd12f38900c376c1
zx <zx@DESKTOP-81P9SJC>
2019-07-18 21:15:58 +0800

    完成退出登录，清除cookie

---
+++ yuanzhan/AspNet/Pages/ExitLogin.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SRV;
+
+namespace UI.Pages
+{
+    public class ExitLoginModel : PageModel
+    {
+        public const string userIdKey = "userId";
+        protected const string userAuth = "PassWord";
+
+        public void OnGet()
+        {
+            string userIdValue;
+            if (HttpContext.Request.Cookies.TryGetValue(userIdKey, out userIdValue))
+            {
+                UserModel model = new UserService().GetById(Convert.ToInt32(userIdValue));
+                if (model != null)
+                {
+                    if (Request.Cookies.TryGetValue(userAuth, out string userAuthValue))
+                    {
+                        if (userAuthValue == model.MD5Password)
+                        {
+                            Response.Cookies.Delete(userAuth);
+                        }
+                    }
+                }
+            }
+         
+        }
+        public void OnPost()
+        {
+        }
+    }
+}
