fc3b2aa61467cdba4fa0785b8a0e0a8e9378b91b
zx <zx@DESKTOP-81P9SJC>
2019-08-01 22:03:27 +0800

    MVC课堂随笔

---
+++ yuanzhan/17bangMVC/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using _17bangMVC.Models.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _17bangMVC.Controllers
+{
+    public class RegisterController: Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return View();
+        }
+    }
+}
