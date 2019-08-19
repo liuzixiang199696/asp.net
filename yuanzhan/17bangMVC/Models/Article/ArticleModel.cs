61bcdde6a9b59a048b5548869c16722810636805
zx <zx@DESKTOP-81P9SJC>
2019-08-19 22:06:11 +0800

    MVC-article模板

---
+++ yuanzhan/17bangMVC/Models/Article/ArticleModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _17bangMVC.Models.Article
+{
+    public class ArticleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string NewPost { get; set; }
+        public List<Post> posts { get; set; }
+    }
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public int ArticleModelId { get; set; }
+        public ArticleModel Article { get; set; }
+    }
+}
